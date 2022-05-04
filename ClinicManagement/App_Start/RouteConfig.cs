ce4231aa7c8fd1e14074d2e2ec753f5b374277ad
AVVBC3744 <AVVBC3744@LAPTOP-HSCFDBCV>
2022-05-04 15:41:21 +0530

    Add project files.

---
+++ ClinicManagement/App_Start/RouteConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ClinicManagement
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            //ATTRIBUTE ROUTE - FOR BIG APPLICATIONS 
+            //action based routing
+           // [Route("ProductType/Product/{mindid}/{maxid}")]
+
+
+
+            //CUSTOM ROUTE 
+            //routes.MapRoute(
+               // name: "Get Particular Doctor",
+              //  url: "DoctorType/Doctor /{ minid}/{ maxid}",
+              //  defaults:new { controller = "DoctorType", action = "Doctor" }
+
+              //  );
+            routes.MapRoute(
+                name: "Default",
+                //DoctorType/Doctor/1/3
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ ClinicManagement/Controllers/DoctorTypeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClinicManagement.Controllers
+{
+    public class DoctorTypeController : Controller
+    {
+        // GET: DoctorType
+        public ActionResult List()
+        {
+            List<Models.DoctorType> DoctorTypeList = new List<Models.DoctorType>()
+            {
+                new Models.DoctorType(){pk_doctor_type_Id=1,Doctor_Description="Cardiologist"},
+                 new Models.DoctorType(){pk_doctor_type_Id=2,Doctor_Description="Physician"},
+                  new Models.DoctorType(){pk_doctor_type_Id=3,Doctor_Description="Dermatologist"},
+                  new Models.DoctorType(){pk_doctor_type_Id=4,Doctor_Description="Pediatrician"},
+                  new Models.DoctorType(){pk_doctor_type_Id=5,Doctor_Description="Ophthlamologist"},
+                  new Models.DoctorType(){pk_doctor_type_Id=6,Doctor_Description="Physician"},
+                  new Models.DoctorType(){pk_doctor_type_Id=7,Doctor_Description="Gynecologists"}
+            };
+            return View(DoctorTypeList);
+        }
+        //for viewiwing routing in MVC
+       // public ActionResult Doctor (int minid, int maxid)
+        //{
+            //return View();
+        //}
+    }
+}
